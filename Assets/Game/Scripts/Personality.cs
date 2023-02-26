dfcdc8c3ba4cc7965b6c693bba106f4fb6ba9274
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-02-26 18:33:32 +0800

    added Personality.cs and ShareLibrary.cs

---
+++ Assets/Game/Scripts/Personality.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.Scripts{
+	[Serializable]
+	public class Personality{
+		public string id;
+		public int loveValue;
+		public List<Personality> disallow;
+		public List<Personality> attractive;
+		public List<Personality> repulsive;
+	}
+}
---
+++ Assets/Game/Scripts/ShareLibrary.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Scripts{
+	public static class ShareLibrary{
+		public static List<string> PersonalityIDs
+				= new List<string>{
+					"Frugal",
+					"Squander",
+				};
+	}
+}
