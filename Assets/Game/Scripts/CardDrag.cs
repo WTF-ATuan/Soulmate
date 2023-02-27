51b9053616f98bb3e2dfdb0a661514a35b2f7817
Atuan <jain60501@gmail.com.tw>
2023-02-27 14:10:48 +0800

    feat drag image

---
+++ Assets/Game/Scripts/CardDrag.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler{
+	private Vector3 _basePos;
+	private Vector3 _vec;
+	private CanvasGroup _canvasGroup;
+
+	private void Awake(){
+		_basePos = transform.localPosition;
+		_canvasGroup = GetComponent<CanvasGroup>();
+	}
+
+	private const float Smooth = 0.1f;
+
+	private void Update(){
+		if(_draging) return;
+		transform.localPosition = (transform.localPosition - _basePos).magnitude < 0.1f
+				? _basePos
+				: Vector3.SmoothDamp(transform.localPosition, _basePos, ref _vec, Smooth);
+	}
+
+	private bool _draging;
+	private Vector2 _deviation;
+
+	public void OnDrag(PointerEventData eventData){
+		transform.localPosition = eventData.position + _deviation;
+	}
+
+	public void OnBeginDrag(PointerEventData eventData){
+		_draging = true;
+		_deviation = (Vector2)transform.localPosition - eventData.position;
+		_canvasGroup.blocksRaycasts = false;
+		_canvasGroup.alpha = 0.7f;
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+		_draging = false;
+		_canvasGroup.blocksRaycasts = true;
+		_canvasGroup.alpha = 1f;
+	}
+
+	public void ResetPosition(){
+		_basePos = transform.localPosition;
+	}
+}
---
+++ Assets/Game/Scripts/CardPresenter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Game.Scripts{
+	public class CardPresenter : MonoBehaviour{
+		[Required] public List<LayoutGroup> inventoryList;
+
+		private void Start(){
+			foreach(var layoutGroup in inventoryList){
+				layoutGroup.OnDropAsObservable().Subscribe(x => DropHandler(x, layoutGroup));
+			}
+		}
+
+		private void DropHandler(PointerEventData eventData, LayoutGroup layout){
+			var draggingObj = eventData.pointerDrag;
+			draggingObj.transform.SetParent(layout.transform, false);
+			draggingObj.GetComponent<CardDrag>().ResetPosition();
+			layout.CalculateLayoutInputHorizontal();
+			layout.CalculateLayoutInputVertical();
+			layout.SetLayoutHorizontal();
+			layout.SetLayoutVertical();
+		}
+	}
+}
