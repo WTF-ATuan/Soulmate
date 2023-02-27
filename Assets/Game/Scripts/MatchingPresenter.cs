3f18c951e063f73e13a42a0e7975419aa0f60a23
Atuan <jain60501@gmail.com.tw>
2023-02-27 14:34:07 +0800

    added matching presenter

---
+++ Assets/Game/Scripts/MatchingPresenter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Scripts{
+	public class MatchingPresenter : MonoBehaviour{
+		[Inject] private readonly PersonalityDataSet _dataSet;
+
+		[ValueDropdown("GetPersonalityID")] public List<string> leftPersonData;
+		[ValueDropdown("GetPersonalityID")] public List<string> rightPersonData;
+
+		[Button]
+		public void MatchMaking(){
+			var rightRules = rightPersonData.Select(personality
+					=> _dataSet.GetBindingData(personality)).ToList();
+
+			var lovePoint = (from personality in leftPersonData
+				from rightRule in rightRules
+				select rightRule.CalculateLoveValue(personality)).Sum();
+			Debug.Log($"lovePoint = {lovePoint}");
+		}
+
+
+		private List<ValueDropdownItem> GetPersonalityID(){
+			return ShareLibrary.PersonalityIDs
+					.Select(personalityID => new ValueDropdownItem(personalityID, personalityID)).ToList();
+		}
+	}
+}
--- Assets/Game/Scripts/PersonalityDataSet.cs
+++ Assets/Game/Scripts/PersonalityDataSet.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using Zenject;
 
 namespace Game.Scripts{
 	[CreateAssetMenu(fileName = "Data", menuName = "Data/Personality", order = 0)]
-	public class PersonalityDataSet : ScriptableObject{
+	public class PersonalityDataSet : ScriptableObjectInstaller<PersonalityDataSet>{
 		public List<PersonalityRules> personalityRuleList;
 		public List<MatchingRules> matchingRulesList;
+
+		public override void InstallBindings(){
+			Container.Bind<PersonalityDataSet>().FromInstance(this);
+		}
+
+		public PersonalityRules GetBindingData(string binding){
+			return personalityRuleList.Find(x =>
+					x.binding.id.Equals(binding));
+		}
 	}
 }
