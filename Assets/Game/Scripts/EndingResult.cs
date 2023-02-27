81b2a75446cf064cc52b88e9ae464ec84925e801
Atuan <jain60501@gmail.com.tw>
2023-02-27 17:19:22 +0800

    connect Data to matching function

---
+++ Assets/Game/Scripts/EndingResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.Scripts{
+	[Serializable]
+	public class EndingResult{
+		public List<string> Person1;
+		public List<string> Person2;
+		public MatchingRules ResultMatching;
+
+		public EndingResult(List<string> person1, List<string> person2, MatchingRules resultMatching){
+			Person1 = person1;
+			Person2 = person2;
+			ResultMatching = resultMatching;
+		}
+	}
+}
--- Assets/Game/Scripts/MatchingPresenter.cs
+++ Assets/Game/Scripts/MatchingPresenter.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Sirenix.OdinInspector;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -10,43 +10,29 @@ namespace Game.Scripts{
 	public class MatchingPresenter : MonoBehaviour{
 		[Inject] private readonly PersonalityDataSet _dataSet;
 
-		[ValueDropdown("GetPersonalityID")] public List<string> leftPersonData;
-		[ValueDropdown("GetPersonalityID")] public List<string> rightPersonData;
-
 		public Image image;
 		[ReadOnly] public List<EndingResult> resultList = new List<EndingResult>();
 
+		private void Start(){
+			EventAggregator.OnEvent<OnSendMatch>().Subscribe(x => {
+				var person1 = x.Data[0];
+				var person2 = x.Data[1];
+				MatchMaking(person1, person2);
+			});
+		}
+
 		[Button]
-		public void MatchMaking(){
-			var rightRules = rightPersonData.Select(personality
+		public void MatchMaking(List<string> person1Data, List<string> person2Data){
+			var rightRules = person2Data.Select(personality
 					=> _dataSet.GetBindingData(personality)).ToList();
 
-			var lovePoint = (from personality in leftPersonData
+			var lovePoint = (from personality in person1Data
 				from rightRule in rightRules
 				select rightRule.CalculateLoveValue(personality)).Sum();
-			var matching = _dataSet.GetCloseMatching(leftPersonData, rightPersonData, lovePoint);
+			var matching = _dataSet.GetCloseMatching(person1Data, person2Data, lovePoint);
 			Debug.Log($"matching = {matching.name}");
 			image.sprite = matching.image;
-			resultList.Add(new EndingResult(leftPersonData, rightPersonData, matching));
-		}
-
-
-		private List<ValueDropdownItem> GetPersonalityID(){
-			return ShareLibrary.PersonalityIDs
-					.Select(personalityID => new ValueDropdownItem(personalityID, personalityID)).ToList();
-		}
-	}
-
-	[Serializable]
-	public class EndingResult{
-		public List<string> Person1;
-		public List<string> Person2;
-		public MatchingRules ResultMatching;
-
-		public EndingResult(List<string> person1, List<string> person2, MatchingRules resultMatching){
-			Person1 = person1;
-			Person2 = person2;
-			ResultMatching = resultMatching;
+			resultList.Add(new EndingResult(person1Data, person2Data, matching));
 		}
 	}
 }
